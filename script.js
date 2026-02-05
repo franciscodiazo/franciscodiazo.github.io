0d4419f74c44590e11a8e76c383a9507cc1cc0ce
franciscodiazo <68307007+franciscodiazo@users.noreply.github.com>
2026-02-05 10:28:30 -0500

    Update texts array in script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const texts = ['Innovación en Educación STEM', 'Diseñando el Futuro Digital', 'Seguridad + Creatividad', 'Transformando la Enseñanza'];
+
+// Typewriter effect function and other code unchanged
+
+function startTypewriterEffect() {
+    // Implementation of typewriter effect
+}
+
+function smoothScroll() {
+    // Implementation of smooth scrolling
+}
+
+function validateActiveLink() {
+    // Implementation of active link validation
+}
